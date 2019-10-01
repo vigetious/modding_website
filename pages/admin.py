6226f7333a930dfba09e88d251b18bbce8504185
Patrick-G-W <patrick.g.ward@protonmail.com>
2019-10-01 16:07:31 +0100

    fixing home page and search

--- pages/admin.py
+++ pages/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.db import models
+from django.forms import Textarea
 
 # Register your models here.
 
@@ -7,6 +9,9 @@ from .models import AdminNews
 
 class AdminNewsAdmin(admin.ModelAdmin):
     list_display = ['adminNewsID', 'adminNewsUserID', 'adminNewsText', 'adminNewsDate']
+    formfield_overrides = {
+        models.CharField: {'widget': Textarea()}
+    }
 
 
 admin.site.register(AdminNews, AdminNewsAdmin)
