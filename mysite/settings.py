653907d73c75eb4f1ce7f588aa9dcd085bf5b422
Patrick-G-W <patrick.g.ward@protonmail.com>
2019-11-14 20:19:56 +0000

    trying to fix security in chrome

--- mysite/settings.py
+++ mysite/settings.py
@@ -249,17 +249,17 @@ SIMPLE_EMAIL_CONFIRMATION_PERIOD = timedelta(days=EMAIL_CONFIRMATION_PERIOD_DAYS
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-#if not DEBUG:
-#    SECURE_HSTS_SECONDS = 3600
-#    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-#    SECURE_HSTS_PRELOAD=True
+if not DEBUG:
+    SECURE_HSTS_SECONDS = 3600
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD=True
 
-#    SECURE_CONTENT_TYPE_NOSNIFF = True
-#    SECURE_BROWSER_XSS_FILTER = True
-#    SESSION_COOKIE_SECURE = True
-#    CSRF_COOKIE_SECURE = True
-#    X_FRAME_OPTIONS = 'DENY'
-#    SECURE_SSL_REDIRECT = True  #developemnt change to False
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    X_FRAME_OPTIONS = 'DENY'
+    SECURE_SSL_REDIRECT = True  #developemnt change to False
 
 
 django_heroku.settings(locals())
