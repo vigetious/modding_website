9fd20347c48cf36ab7f2b2cf53b8b68d55434543
Patrick Ward <44092756+vigetious@users.noreply.github.com>
2019-11-14 22:06:39 +0000

    Delete secret.py

--- mysite/secret.py
+++
@@ -1,9 +0,0 @@
-access_key_id = 'AKIARHRBXQJOSG6TN37J'
-secret_access_key = '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'
-bucket_name = 'ddlc-modding-static'
-django_secret_key = '5o@g-^17+r3-1ox=*(-^7h@@zb((wm@d1nb+w8oi$8s95t%a4a'
-
-captcha_site_key = '6LdwaroUAAAAAEbWMBpRabK1zdYr1rvN6_2BFrU8'
-captcha_secret_key = '6LdwaroUAAAAAJwX2kcNPo9vLA33scQAF3cSaRNx'
-
-email_host_password = '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'
