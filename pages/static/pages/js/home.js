48ccab0f4be5884c8728072ffe853c2caeced763
Patrick-G-W <patrick.g.ward@protonmail.com>
2020-02-01 11:20:19 +0000

    Improved homepage, replaced discord link with featured mods

--- pages/static/pages/js/home.js
+++ pages/static/pages/js/home.js
@@ -1,12 +1,13 @@
 let panelOpen = 0;
+//var firstFeatured = {};
+var firstFeatured = undefined;
+var firstRandom = undefined;
 
 function showPanel(modID) {
     $('#mod' + panelOpen).css("display", "none");
     panelOpen = modID;
 
     $('#mod' + modID).css("display", "inline-block");
-    console.log(modID);
-    console.log(panelOpen);
 }
 
 function changePanel(modID, modName, avatarURL, modShortDescription, modAuthor, modDate, modPlayTimeHours, modStatus, modRating, tags, searchURL, modPreviewImage1, modPreviewImage2, modPreviewImage3, modPreviewImage4, modPreviewImage5) {
@@ -51,5 +52,185 @@ function changePanel(modID, modName, avatarURL, modShortDescription, modAuthor, 
         $('#modPreviewImage5').attr("src", modPreviewImage5).attr("style", "display: inline-block");
     } else {
         $('#modPreviewImage5').hide();
+    }
+}
+
+
+function changeFeatured(modID, avatarURL, modShortDescription, tags, searchURL, modPreviewImage1, modPreviewImage2, modPreviewImage3, modPreviewImage4, modPreviewImage5) {
+    if (document.readyState === 'loading') {
+        document.addEventListener('DOMContentLoaded', afterLoaded);
+    } else {
+        afterLoaded();
+    }
+
+    function afterLoaded() {
+        $('#featuredAvatar').attr("src", avatarURL);
+        $('#randomModShortDescription').text(modShortDescription);
+        $('#featuredAvatarLink').attr("href", "/mod/" + modID);
+        $('#featuredTags').empty();
+
+        hidePreviewImages();
+
+        for (i = 0; i < tags.length; i++) {
+            $('#featuredTags').append("<a class='tag' href='" + searchURL + "?q=&sortBy=newest&tags=" + tags[i] + "&dateBy=0'>" + tags[i] + "</a> ");
+        }
+        if (modPreviewImage1 !== "null") {
+            $('#featuredModBarPreviewImage1').attr("src", modPreviewImage1).attr("style", "display: inline-block");
+            $('#randomModPreviewImage1').attr("src", modPreviewImage1);
+        } else {
+            $('#featuredModBarPreviewImage1').hide();
+        }
+        if (modPreviewImage2 !== "null") {
+            $('#featuredModBarPreviewImage2').attr("src", modPreviewImage2).attr("style", "display: inline-block");
+            $('#randomModPreviewImage2').attr("src", modPreviewImage2);
+        } else {
+            $('#featuredModBarPreviewImage2').hide();
+        }
+        if (modPreviewImage3 !== "null") {
+            $('#featuredModBarPreviewImage3').attr("src", modPreviewImage3).attr("style", "display: inline-block");
+            $('#randomModPreviewImage3').attr("src", modPreviewImage3);
+        } else {
+            $('#featuredModBarPreviewImage3').hide();
+        }
+        if (modPreviewImage4 !== "null") {
+            $('#featuredModBarPreviewImage4').attr("src", modPreviewImage4).attr("style", "display: inline-block");
+            $('#randomModPreviewImage4').attr("src", modPreviewImage4);
+        } else {
+            $('#featuredModBarPreviewImage4').hide();
+        }
+        if (modPreviewImage5 !== "null") {
+            $('#featuredModBarPreviewImage5').attr("src", modPreviewImage5).attr("style", "display: inline-block");
+            $('#randomModPreviewImage5').attr("src", modPreviewImage5);
+        } else {
+            $('#featuredModBarPreviewImage5').hide();
+        }
+        if (modPreviewImage1 !== "null") {
+            $('#randomModPreviewImage1').attr("style", "display: inline-block").attr("class", "mySlides thumb selected");
+        } else if (modPreviewImage2 !== "null") {
+            $('#randomModPreviewImage2').attr("style", "display: inline-block").attr("class", "mySlides thumb selected");
+        } else if (modPreviewImage3 !== "null") {
+            $('#randomModPreviewImage3').attr("style", "display: inline-block").attr("class", "mySlides thumb selected");
+        } else if (modPreviewImage4 !== "null") {
+            $('#randomModPreviewImage4').attr("style", "display: inline-block").attr("class", "mySlides thumb selected");
+        } else if (modPreviewImage5 !== "null") {
+            $('#randomModPreviewImage5').attr("style", "display: inline-block").attr("class", "mySlides thumb selected");
+        }
+
+        $('#featuredMod' + modID).css("background-color", "");
+    }
+}
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', afterLoaded);
+} else {
+    afterLoaded();
+}
+
+function afterLoaded() {
+    $('.screenshotVideoContent').click(function () {
+        $(document).keydown(function(e) {
+            switch (e.key) {
+                case "ArrowLeft":
+                    leftButton();
+                    break;
+                case "ArrowRight":
+                    rightButton();
+                    break;
+                default: return
+            }
+            e.preventDefault();
+        });
+    });
+
+    $('.search').click(function () {
+        $(document).off('keydown');
+    });
+
+    $('.screenshotVideoContent').hover(function() {
+        $('#leftButton1').toggleClass("hiddenA");
+        $('#rightButton1').toggleClass("hiddenA");
+    });
+
+    $('.leftButton').hover(function () {
+        $('.leftButton').toggleClass("hiddenB").toggleClass("hiddenA");
+    });
+    $('.rightButton').hover(function () {
+        $('.rightButton').toggleClass("hiddenB").toggleClass("hiddenA");
+    });
+
+    for (var x in $(".mySlides")) {
+        if ($(".mySlides")[x].src) {
+            var id = $(".mySlides")[x].id;
+            $("#" + id).attr("style", "display: inline-block");
+            break;
+        }
+    }
+
+    $('.leftButton').click(function () {
+        leftButton()
+    });
+    $('.rightButton').click(function () {
+        rightButton();
+    }); //idk why this isnt working
+
+    function leftButton() {
+        var prev = $(".selected").prev();
+        var img = prev.attr('src');
+        if (img) {
+            $(".mySlides").removeClass('selected').hide();
+            prev.attr("style", "display: inline-block");
+            prev.addClass('selected');
+        }
+    }
+
+    function rightButton() {
+        var next = $(".selected").next();
+        var img = next.attr('src');
+        if (img) {
+            $(".mySlides").removeClass('selected').hide();
+            next.attr("style", "display: inline-block");
+            next.addClass('selected');
+        }
+    }
+}
+
+function hidePreviewImages() {
+    $('#randomModPreviewImage1').hide().attr("src", "").attr("class", "mySlides thumb");
+    $('#randomModPreviewImage2').hide().attr("src", "").attr("class", "mySlides thumb");
+    $('#randomModPreviewImage3').hide().attr("src", "").attr("class", "mySlides thumb");
+    $('#randomModPreviewImage4').hide().attr("src", "").attr("class", "mySlides thumb");
+    $('#randomModPreviewImage5').hide().attr("src", "").attr("class", "mySlides thumb");
+}
+
+
+function currentDiv(n) {
+    showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex-2].style.display = "block";
+}
+
+function calculateFirstFeatured(BarOrVideo, modNumber) {
+    if (document.readyState === 'loading') {
+        document.addEventListener('DOMContentLoaded', afterLoaded);
+    } else {
+        afterLoaded();
+    }
+
+    function afterLoaded() {
+        if (BarOrVideo === true && firstFeatured === undefined) {
+            firstFeatured = modNumber;
+            $('#randomModPreviewImage' + modNumber).attr("style", "display: inline-block");
+        } else if (BarOrVideo === false) {
+            $('#featuredModBarPreviewImage' + modNumber).attr("style", "display: inline-block");
+        }
     }
 }
--- pages/views.py
+++ pages/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 from django.apps import apps
 from taggit.models import Tag
+from django.db.models import Q
 
 from mod.models import Mod
 from accounts.models import User
@@ -12,14 +13,20 @@ import pdb, random
 
 
 def home(request):
-    randomMod = Mod.objects.get(modID=24)#random.choice(Mod.objects.all().filter(modShow=True))
+    randomMods = Mod.objects.all()#make sure only mods with images show#.get(modID=24)
+    featuredMods = randomMods.filter(modShow=True).filter(modNSFW=False).filter(~Q(modAvatar="") & (~Q(modPreviewImage1="") | ~Q(modPreviewImage2="") | ~Q(modPreviewImage3="") | ~Q(modPreviewImage4="") | ~Q(modPreviewImage5="")))
     newMods = Mod.objects.filter(modShow=True).order_by('-modDate')[:8]
     ratedMods = Mod.objects.filter(modShow=True).order_by('-modRating')[:8]
     # reviewedMods = Mod.objects.filter(modShow=True).order_by('-modReviewCount')[:12]
     tags = Tag.objects.all()
     reviewedMods = sorted(Mod.objects.filter(modShow=True), key=lambda t: t.modReviewCount, reverse=True)[:8]  # order by review count using sorted
     adminNews = AdminNews.objects.all().order_by('-adminNewsDate')
-    return render(request, 'pages/home.html', {'newMods': newMods, 'ratedMods': ratedMods, 'reviewedMods': reviewedMods, 'adminNews': adminNews, 'randomMod': randomMod})
+    return render(request, 'pages/home.html', {'newMods': newMods, 'ratedMods': ratedMods, 'reviewedMods': reviewedMods, 'adminNews': adminNews, 'randomMod': featuredMods})
+
+
+def news(request):
+    adminNews = AdminNews.objects.all().order_by('-adminNewsDate')
+    return render(request, 'pages/news.html', {'adminNews': adminNews})
 
 
 def supportHome(request):
