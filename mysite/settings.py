15dd7703770df0189f1e08f5c50fa5fe17b8e15c
Patrick-G-W <patrick.g.ward@protonmail.com>
2020-07-29 21:19:07 +0100

    Updating core settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 if DEBUG:
     SECRET_KEY = 1234567890
